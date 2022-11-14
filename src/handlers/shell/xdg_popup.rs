c04f903b26363f787337a3cdec6a0b2b8feb9ab4
Ashley Wulber <ashley@system76.com>
2022-11-14 20:22:12 +0100

    fix: handle popup done event in order of created popup

--- src/handlers/shell/xdg_popup.rs
+++ src/handlers/shell/xdg_popup.rs
@@ -1,5 +1,5 @@
 use crate::{
-    event_loop::state::{SctkState, SctkSurface},
+    event_loop::state::{self, SctkState, SctkSurface},
     sctk_event::{PopupEventVariant, SctkEvent},
 };
 use sctk::{delegate_xdg_popup, reexports::client::Proxy, shell::xdg::popup::PopupHandler};
@@ -42,25 +42,39 @@ impl<T: Debug> PopupHandler for SctkState<T> {
         _qh: &sctk::reexports::client::QueueHandle<Self>,
         popup: &sctk::shell::xdg::popup::Popup,
     ) {
-        let sctk_popup = match self
-            .popups
-            .iter()
-            .position(|s| s.popup.wl_surface().id() == popup.wl_surface().id())
-        {
-            Some(p) => self.popups.remove(p),
-            None => return,
-        };
-        self.sctk_events.push(SctkEvent::PopupEvent {
-            variant: PopupEventVariant::Done,
-            id: popup.wl_surface().id(),
-            toplevel_id: sctk_popup.toplevel.id(),
-            parent_id: match &sctk_popup.parent {
-                SctkSurface::LayerSurface(s) => s.id(),
-                SctkSurface::Window(s) => s.id(),
-                SctkSurface::Popup(s) => s.id(),
-            },
-        })
-        // TODO popup cleanup
+        let mut to_destroy = vec![popup.wl_surface().id()];
+        while let Some(id) = to_destroy.last().cloned() {
+            if let Some(i) = self
+                .popups
+                .iter()
+                .position(|p| p.popup.wl_surface().id() == id)
+            {
+                let popup_to_destroy = self.popups.remove(i);
+                match &popup_to_destroy.parent.clone() {
+                    state::SctkSurface::LayerSurface(_) | state::SctkSurface::Window(_) => {
+                        popup_to_destroy.popup.xdg_popup().destroy();
+                        self.sctk_events.push(SctkEvent::PopupEvent {
+                            variant: PopupEventVariant::Done,
+                            toplevel_id: popup_to_destroy.toplevel.id(),
+                            parent_id: popup_to_destroy.parent.wl_surface().id(),
+                            id: popup_to_destroy.popup.wl_surface().id(),
+                        });
+                        to_destroy.pop();
+                    }
+                    state::SctkSurface::Popup(popup_to_destroy_first) => {
+                        self.popups.push(popup_to_destroy);
+                        let popup_to_destroy_first = self
+                            .popups
+                            .iter()
+                            .find(|p| p.popup.wl_surface() == popup_to_destroy_first)
+                            .unwrap();
+                        to_destroy.push(popup_to_destroy_first.popup.wl_surface().id());
+                    }
+                }
+            } else {
+                to_destroy.pop();
+            }
+        }
     }
 }
 delegate_xdg_popup!(@<T: 'static + Debug> SctkState<T>);
